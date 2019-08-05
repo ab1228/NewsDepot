c1da2858fd73d80af82b97cc2ea9968342ee9ab5
ab1228 <alexb8734@gmail.com>
2019-08-05 17:17:43 -0500

    added controllers folder with js

--- server.js
+++ server.js
@@ -29,45 +29,7 @@ app.get("/", function (req, res) {
 
 
 
-// // Route 1
-// // =======
-// // This route will retrieve all of the data from the scrapedData collection as a json (this will be populated
-// // by the data you scrape using the next route)
-// app.get("/all", function (req, res) {
 
-//     db.scrapeData.find({}, function (error, found) {
-//         // Log any errors if the server encounters one
-//         if (error) {
-//             console.log(error);
-//         }
-//         // Otherwise, send the result of this query to the browser
-//         else {
-//             res.json(found);
-//         }
-//     });
-// });
-// / --- HANDLEBARS ----/ /
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-// // ROUTE 2
-// // =======
-// // server will scrape data from the site and save it to MongoDB.
-// app.get("/scrape", function (req, res) {
-//     axios.get("https://www.nytimes.com").then(function (response) {
-//         var $ = cheerio.load(response.data);
-//         var results = [];
-//         $("article").each(function (i, element) {
-//             var title = $(element).children().text();
-//             var link = $(element).find("a").attr("href");
-//             results.push({
-//                 title: title,
-//                 link: link
-//             });
-//         });
-//         db.scrapedData.insert(results);
-//         res.json(results);
-//     });
-// });
 
 // LISTENER
 app.listen(3050, function () {
